fce9483225fa4f248ca66826e37f268cc706f718
YazanT007 <yazantaha1@gmail.com>
2025-08-30 14:16:07 +1000

    Changed the PORT so Render can use its own port.

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const cors = require("cors");
 require("dotenv").config();
 
 const app = express();
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 
 app.use(cors());
 app.use(bodyParser.json());
@@ -18,8 +18,8 @@ app.post("/send", async (req, res) => {
         let transporter = nodemailer.createTransport({
             service: "gmail",
             auth: {
-                user: process.env.EMAIL_USER,  
-                pass: process.env.EMAIL_PASS   
+                user: process.env.EMAIL_USER,
+                pass: process.env.EMAIL_PASS
             }
         });
 
